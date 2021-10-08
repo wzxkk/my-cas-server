11ba213abae152ef9e69f90f907e60fbe6ef93e3
wzx <377322994@qq.com>
2021-10-08 15:45:40 +0800

    启动类

---
+++ src/main/java/org/wzx/mycasserver/MyCasServerApplication.java
@@ -0,0 +1,13 @@
+package org.wzx.mycasserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCasServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyCasServerApplication.class, args);
+    }
+
+}
