06ca17893f8adb77a86d472c36b5c9c9c8a855db
wzx <377322994@qq.com>
2021-10-13 14:48:47 +0800

    权限系统开发

--- src/main/java/org/wzx/mycasserver/service/BaseService.java
+++ src/main/java/org/wzx/mycasserver/service/BaseService.java
@@ -6,6 +6,7 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @description: 服务基础接口
  * @author: 鱼头(韦忠幸)
  * @since: 2020-06-28
+ * @version: 0.0.1
  */
 public interface BaseService<T> extends IService<T> {
     default boolean checkInsert(T entity) { return true; }
